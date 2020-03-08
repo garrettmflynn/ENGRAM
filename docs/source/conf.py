7d7ec126baaa69e429897980c08df2d747e23f4f
Garrett <gflynn@usc.edu>
2020-03-08 14:53:38 -0700

    Changed conf.py for RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 sys.path.insert(1, os.path.abspath('../../'))
 
-package_path = os.path.abspath('../..')
+#package_path = os.path.abspath('../..')
 # os.environ['PYTHONPATH'] = ':'.join((package_path, os.environ.get('PYTHONPATH', '')))
 
 # -- Project information -----------------------------------------------------
