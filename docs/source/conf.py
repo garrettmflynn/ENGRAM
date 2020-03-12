f48714b43b8303133fbbce37bb73be07f66dfbfd
Garrett <gflynn@usc.edu>
2020-03-12 09:37:28 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,7 +4,7 @@
 
 import os
 import sys
-sys.path.insert(1, os.path.abspath('../../'))
+sys.path.insert(1, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
