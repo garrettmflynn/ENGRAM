f0fab236cd8ab3aa15914e6d598e3096c9efd1e0
Garrett <gflynn@usc.edu>
2020-03-12 09:06:36 -0700

    Fixed autodoc (hopefully)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,21 +1,10 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
 
 import os
 import sys
 sys.path.insert(1, os.path.abspath('../../'))
-
-#package_path = os.path.abspath('../..')
-# os.environ['PYTHONPATH'] = ':'.join((package_path, os.environ.get('PYTHONPATH', '')))
 
 # -- Project information -----------------------------------------------------
 
@@ -32,28 +21,19 @@ version = '.'.join(release.split('.')[:2])
 
 master_doc = 'index'
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-
 extensions = ['sphinxcontrib.bibtex','sphinx.ext.autodoc']
 
 # ,'jupyter_sphinx.execute']
 
-autodoc_mock_imports = ['engram','tensorflow','vispy']
+autodoc_mock_imports = ['tensorflow','vispy']
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
     'canonical_url': '',
-    'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
     'logo_only': True,
     'display_version': True,
     'prev_next_buttons_location': 'bottom',
@@ -67,15 +47,10 @@ html_theme_options = {
     'titles_only': False
 }
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
 html_logo = 'images/engramlogo.png'
 
-# If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 html_show_sphinx = True
 
-# If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
 html_show_copyright = True
--- engram/test/__init__.py
+++ engram/test/__init__.py
@@ -3,4 +3,5 @@
 The subdirectory declarativetest contains tests for engram.declarative
 The subdirectory proceduraltest contains tests for engram.procedural
 The subdirectory episodictest contains tests for engram.episodic
+The subdirectory workingtest contains tests for engram.working
 """
